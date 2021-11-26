7100591d301bc7069a9ac33fa5fb359d71a8a3bd
Miao JinZhou <1962247851@qq.com>
2021-11-26 15:02:57 +0800

    feat(ordinaryroad-ui): 优化axios请求响应拦截器，请求header增加satoken字段，发生错误使用dialog和snackbar提示用户

--- ordinaryroad-ui/plugins/axios/index.js
+++ ordinaryroad-ui/plugins/axios/index.js
@@ -1,60 +1,42 @@
+import Cookies from 'js-cookie'
 import errorCode from './errorCode'
 
 // Create a custom axios instance
-export default function ({ $axios, route, app }, inject) {
-  const token = 'token'
+export default function (context, inject) {
+  // $dialog和$snackbar必须通过context.$xxx方式调用
+  const { $axios, route, app } = context
   const { store, router } = app
   // 请求拦截器
   $axios.onRequest((config) => {
     // 将获取到token加入到请求头中
-    config.headers.common.Authorization = token
+    config.headers.common.satoken = Cookies.get('satoken') || ''
   })
 
   // 响应拦截器
   $axios.interceptors.response.use((res) => {
     // 未设置状态码则默认成功状态
-    const code = res.data.errorCode || 200
+    const code = res.data.code || 200
     // 获取错误信息
-    const msg = errorCode[code] || res.data.errorMsg || errorCode.default
-    if (code === 2001) {
-      alert('登录状态已过期，即将跳转登录页面')
-      store.dispatch('user/LogOut').then(() => {
-        router.push({ path: '/dashboard/user/login', query: { redirect: route.fullPath } })
-      })
-      /**
-         MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', {
-          confirmButtonText: '重新登录',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }
-         ).then(() => {
-        store.dispatch('LogOut').then(() => {
-          location.href = '/index';
+    const msg = errorCode[code] || res.data.details || res.data.msg || errorCode.default
+    if (code === 3001) {
+      context.$dialog({
+        persistent: true,
+        title: '系统提示',
+        content: '登录状态已过期，您可以继续留在该页面，或者重新登录。',
+        confirmText: '重新登录'
+      }).then((value) => {
+        // TODO 跳转登录页面
+        alert('跳转登录页面')
+        store.dispatch('user/LogOut').then(() => {
+          router.push({ path: '/dashboard/user/login', query: { redirect: route.fullPath } })
         })
       })
-         **/
-      return Promise.reject(new Error(msg))
     } else if (code === 500) {
-      alert(msg)
-      /**
-         Message({
-        message: msg,
-        type: 'error'
-      })
-         **/
-      return Promise.reject(new Error(msg))
+      context.$snackbar.error(msg)
     } else if (code !== 200) {
-      alert(msg)
-      /**
-         Notification.error({
-        title: msg
-      })
-         **/
-      // return Promise.reject('error')
-      return res.data
-    } else {
-      return res.data
+      context.$snackbar.error(msg)
     }
+    return res.data
   },
   (error) => {
     console.log('err' + error)
@@ -66,14 +48,8 @@ export default function ({ $axios, route, app }, inject) {
     } else if (message.includes('Request failed with status code')) {
       message = '系统接口' + message.substr(message.length - 3) + '异常'
     }
-    alert(message)
-    /**
-       Message({
-      message: message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-       **/
+    context.$snackbar.error(message)
     return Promise.reject(error)
-  })
+  }
+  )
 }
