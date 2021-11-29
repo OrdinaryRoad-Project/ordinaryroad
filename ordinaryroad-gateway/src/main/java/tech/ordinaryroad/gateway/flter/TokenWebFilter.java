ca16a8ffa31f7c217205c65a6c46a89f0ff3e8ac
Miao JinZhou <1962247851@qq.com>
2021-11-29 10:44:16 +0800

    perf:(ordinaryroad-gateway)

--- ordinaryroad-gateway/src/main/java/tech/ordinaryroad/gateway/flter/TokenWebFilter.java
+++ ordinaryroad-gateway/src/main/java/tech/ordinaryroad/gateway/flter/TokenWebFilter.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 苗锦洲
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package tech.ordinaryroad.gateway.flter;
 
 import cn.dev33.satoken.oauth2.logic.SaOAuth2Util;
@@ -8,14 +31,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+import tech.ordinaryroad.commons.core.base.result.Result;
 import tech.ordinaryroad.commons.core.utils.server.FilterRequestResponseUtil;
+import tech.ordinaryroad.commons.core.utils.server.ServletUtils;
 
 
 /**
@@ -29,7 +53,7 @@ import tech.ordinaryroad.commons.core.utils.server.FilterRequestResponseUtil;
 @Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Component
-public class ModifyRequestHeaderGlobalFilter implements WebFilter {
+public class TokenWebFilter implements WebFilter {
 
     @NotNull
     @Override
@@ -46,8 +70,7 @@ public class ModifyRequestHeaderGlobalFilter implements WebFilter {
             try {
                 accessTokenModel = SaOAuth2Util.checkAccessToken(accessToken);
             } catch (Exception e) {
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
+                return ServletUtils.webFluxResponseWriter(exchange.getResponse(), Result.fail(e.getMessage()));
             }
             String tokenValue = StpUtil.getTokenValueByLoginId(accessTokenModel.loginId);
             // 将Authorization解析为新header：satoken
--- ordinaryroad-gateway/src/main/java/tech/ordinaryroad/gateway/flter/ValidateCodeWebFilter.java
+++ ordinaryroad-gateway/src/main/java/tech/ordinaryroad/gateway/flter/ValidateCodeWebFilter.java
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 苗锦洲
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package tech.ordinaryroad.gateway.flter;
 
 import cn.hutool.core.io.IoUtil;
@@ -25,12 +48,13 @@ import java.nio.charset.StandardCharsets;
 /**
  * 验证码过滤器
  *
- * @author ruoyi
+ * @author mjz
+ * @date 2021/11/29
  */
 @RequiredArgsConstructor
 @Order
 @Component
-public class ValidateCodeFilter implements WebFilter {
+public class ValidateCodeWebFilter implements WebFilter {
 
     private static final String[] VALIDATE_URL = new String[]{"/login", "/upms/user/register"};
     private static final String OR_NUMBER = "orNumber";
