fcba3a43bf12bd1d7bc3eb2ba02321ec8132ea65
Miao JinZhou <1962247851@qq.com>
2021-11-30 16:12:55 +0800

    feat(ordinaryroad-ui): 增加表单校验规则$rules到nuxt上下文

--- ordinaryroad-ui/plugins/ordinaryroad/index.js
+++ ordinaryroad-ui/plugins/ordinaryroad/index.js
@@ -3,6 +3,16 @@ import Vue from 'vue'
 import constants from './constants'
 /* 自定义工具类 */
 import util from './utils/util'
+import rules from './utils/rules/index'
 
 Vue.prototype.$constants = constants
 Vue.prototype.$util = util
+
+export default function (context, inject) {
+  const { app } = context
+  const i18n = app.i18n
+  rules.init(i18n.$t)
+
+  // 将自定义form rules注入nuxt上下文 $rules
+  inject('rules', rules)
+}
---
+++ ordinaryroad-ui/plugins/ordinaryroad/utils/rules/index.js
@@ -0,0 +1,20 @@
+let $t = null
+
+export default {
+  init: (t) => {
+    $t = $t || t
+  },
+  required: value => !!value || $t('required'),
+  min6Chars: value => value.length >= 6 || $t('minNChars', [6]),
+  max16Chars: value => value.length <= 16 || $t('maxNChars', [16]),
+  max25chars: v => v.length <= 25 || $t('maxNChars', [25]),
+  max100Chars: value => value.length <= 100 || $t('maxNChars', [100]),
+  email: (value) => {
+    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return pattern.test(value) || $t('invalidEmail')
+  },
+  password: (value) => {
+    const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}$/
+    return pattern.test(value) || $t('invalidPassword')
+  }
+}
