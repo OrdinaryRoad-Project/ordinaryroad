33ac911b82d11d8b2a1e51ecee261c59ebae13c7
Miao JinZhou <1962247851@qq.com>
2021-11-30 15:42:56 +0800

    feat(ordinaryroad-ui): 增加优化登录注册验证码相关API

--- ordinaryroad-ui/plugins/api/upms/user/index.js
+++ ordinaryroad-ui/plugins/api/upms/user/index.js
@@ -4,6 +4,10 @@ export default {
   initAxios (axios) {
     $axios = $axios || axios
   },
+  register: ({ code, password, email, username }) => {
+    const data = { code, password, email, username }
+    return $axios({ url: '/upms/user/register', method: 'post', data })
+  },
   list: (offset, limit, username) => {
     const data = { offset, limit, username }
     return $axios({ url: '/upms/user/list', method: 'post', data })
