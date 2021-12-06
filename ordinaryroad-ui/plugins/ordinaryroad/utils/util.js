40ba785eb4e8bd7636fbd71f24d641ec4563d462
Miao JinZhou <1962247851@qq.com>
2021-12-06 21:17:54 +0800

    feat(ordinaryroad-ui): 增加判断两个对象中的值是否完全相等方法

--- ordinaryroad-ui/plugins/ordinaryroad/utils/util.js
+++ ordinaryroad-ui/plugins/ordinaryroad/utils/util.js
@@ -160,6 +160,30 @@ function uuid () {
   return s.join('')
 }
 
+/**
+ * 对比两个对象的值是否完全相等
+ * @param a 对象A
+ * @param b 对象B
+ * @returns {boolean}
+ */
+function objectEquals (a, b) {
+  // 取对象a和b的属性名
+  const aProps = Object.keys(a)
+  const bProps = Object.keys(b)
+  // 判断属性名的length是否一致
+  if (aProps.length !== bProps.length) {
+    return false
+  }
+  // 循环取出属性名，再判断属性值是否一致
+  for (let i = 0; i < aProps.length; i++) {
+    const propName = aProps[i]
+    if (a[propName] !== b[propName]) {
+      return false
+    }
+  }
+  return true
+}
+
 module.exports = {
   formatSeconds,
   formatTime,
@@ -169,5 +193,6 @@ module.exports = {
   query,
   remove,
   contain,
-  uuid
+  uuid,
+  objectEquals
 }
