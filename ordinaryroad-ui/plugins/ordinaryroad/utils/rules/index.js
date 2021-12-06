0b52d028cc905473ffecb92cb425064d63934a91
Miao JinZhou <1962247851@qq.com>
2021-12-06 21:51:44 +0800

    perf(ordinaryroad-ui): 最大长度规则移除必填验证

--- ordinaryroad-ui/plugins/ordinaryroad/utils/rules/index.js
+++ ordinaryroad-ui/plugins/ordinaryroad/utils/rules/index.js
@@ -6,10 +6,12 @@ export default {
   },
   required: value => !!value || $t('required'),
   min6Chars: value => (!value || value.length >= 6) || $t('minNChars', [6]),
-  max10Chars: value => value.length <= 10 || $t('maxNChars', [10]),
-  max16Chars: value => value.length <= 16 || $t('maxNChars', [16]),
-  max25chars: value => value.length <= 25 || $t('maxNChars', [25]),
-  max100Chars: value => value.length <= 100 || $t('maxNChars', [100]),
+  max10Chars: value => (!value || value.length <= 10) || $t('maxNChars', [10]),
+  max16Chars: value => (!value || value.length <= 16) || $t('maxNChars', [16]),
+  max25Chars: value => (!value || value.length <= 25) || $t('maxNChars', [25]),
+  max32Chars: value => (!value || value.length <= 32) || $t('maxNChars', [32]),
+  max100Chars: value => (!value || value.length <= 100) || $t('maxNChars', [100]),
+  max200Chars: value => (!value || value.length <= 200) || $t('maxNChars', [200]),
   email: (value) => {
     const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return pattern.test(value) || $t('invalidEmail')
