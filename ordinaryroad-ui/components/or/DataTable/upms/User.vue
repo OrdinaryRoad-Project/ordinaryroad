956e8965701ba7a427cee0bfbf040d0c691ae8e0
Miao JinZhou <1962247851@qq.com>
2021-12-08 18:11:31 +0800

    feat(ordinaryroad-ui): 封装VDataTable
    1. props: selectReturnObject, singleSelect, showSelect, showActionsWhenSelecting, tableHeaders
    2. events: insertItem, deleteItem(item,index), editItem(item,index), getItems(options,offset,limit), itemsSelected(items)
    3. methods: deleteSuccessfully, deleteFailed, loadSuccessfully(items,totalItems), loadFinish

--- ordinaryroad-ui/components/or/DataTable/upms/User.vue
+++ ordinaryroad-ui/components/or/DataTable/upms/User.vue
@@ -1,127 +1,64 @@
 <template>
   <div>
-    <v-data-table
-      :headers="headers"
-      :items="dataTableParams.items"
-      :options.sync="options"
-      :server-items-length="dataTableParams.totalItems"
-      :loading="dataTableParams.loading"
-      :items-per-page="20"
-      :footer-props="{
-        showFirstLastPage: true,
-        firstIcon: 'mdi-page-first',
-        lastIcon: 'mdi-page-last',
-        itemsPerPageOptions:[ 10, 20, 50, 100 ]
-      }"
+    <or-base-data-table
+      ref="dataTable"
+      :single-select="singleSelect"
+      :select-return-object="selectReturnObject"
+      :show-select="showSelect"
+      :show-actions-when-selecting="showActionsWhenSelecting"
+      :table-headers="headers"
+      @getItems="onGetItems"
+      @insertItem="onInsertItem"
+      @deleteItem="onDeleteItem"
+      @editItem="onEditItem"
+      @itemsSelected="onItemsSelected"
     >
-      <template #top>
-        <v-form ref="searchForm">
-          <v-row align="center">
-            <v-col
-              cols="6"
-              lg="3"
-              md="4"
-            >
-              <v-text-field
-                v-model="searchParams.email"
-                dense
-                outlined
-                clearable
-                hide-details="auto"
-                maxlength="10"
-                :label="$t('email')"
-              />
-            </v-col>
-            <v-col
-              cols="6"
-              lg="3"
-              md="4"
-            >
-              <v-text-field
-                v-model="searchParams.orNumber"
-                dense
-                outlined
-                clearable
-                hide-details="auto"
-                maxlength="10"
-                :label="$t('orNumber')"
-              />
-            </v-col>
-            <v-col
-              cols="6"
-              lg="3"
-              md="4"
-            >
-              <v-text-field
-                v-model="searchParams.username"
-                dense
-                outlined
-                clearable
-                hide-details="auto"
-                maxlength="10"
-                :label="$t('username')"
-              />
-            </v-col>
-            <v-col
-              cols="6"
-              lg="3"
-              md="4"
-            >
-              <v-btn
-                small
-                color="primary"
-                outlined
-                @click="getItems"
-              >
-                <v-icon>mdi-magnify</v-icon>
-                {{ $t('search') }}
-              </v-btn>
-              <v-btn
-                small
-                outlined
-                @click="resetSearch"
-              >
-                <v-icon>mdi-refresh</v-icon>
-                {{ $t('reset') }}
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-form>
-        <v-row class="mt-2">
-          <v-col
-            cols="12"
-            md="4"
-          >
-            <v-btn
-              outlined
-              color="primary"
-              dark
-              @click="insertItem"
-            >
-              <v-icon>mdi-plus</v-icon>
-              {{ $t('insert') }}
-            </v-btn>
-            <v-btn
-              outlined
-              color="success"
-              dark
-              @click="getItems"
-            >
-              <v-icon>mdi-reload</v-icon>
-              {{ $t('refresh') }}
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-divider class="mt-2" />
-      </template>
-
-      <template
-        v-if="!$vuetify.breakpoint.xs"
-        #[`header.actions`]="{ header }"
-      >
-        <v-sheet elevation="1">
-          {{ header.text }}
-        </v-sheet>
+      <template #searchFormBody>
+        <v-col
+          cols="6"
+          lg="3"
+          md="4"
+        >
+          <v-text-field
+            v-model="searchParams.email"
+            dense
+            outlined
+            clearable
+            hide-details="auto"
+            maxlength="10"
+            :label="$t('email')"
+          />
+        </v-col>
+        <v-col
+          cols="6"
+          lg="3"
+          md="4"
+        >
+          <v-text-field
+            v-model="searchParams.orNumber"
+            dense
+            outlined
+            clearable
+            hide-details="auto"
+            maxlength="10"
+            :label="$t('orNumber')"
+          />
+        </v-col>
+        <v-col
+          cols="6"
+          lg="3"
+          md="4"
+        >
+          <v-text-field
+            v-model="searchParams.username"
+            dense
+            outlined
+            clearable
+            hide-details="auto"
+            maxlength="10"
+            :label="$t('username')"
+          />
+        </v-col>
       </template>
 
       <template #[`item.enabled`]="{ item }">
@@ -133,71 +70,25 @@
         />
       </template>
 
-      <template #[`item.createdTime`]="{ item }">
-        {{ $dayjs(item.createdTime).format() }}
+      <template #moreActions="{item}">
+        <v-list-item @click="resetItemPassword(item)">
+          <v-list-item-avatar size="16">
+            <v-icon small>
+              mdi-lock
+            </v-icon>
+          </v-list-item-avatar>
+          <v-list-item-title>{{ $t('resetPassword') }}</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="updateItemRoles(item)">
+          <v-list-item-avatar size="16">
+            <v-icon small>
+              mdi-account-multiple
+            </v-icon>
+          </v-list-item-avatar>
+          <v-list-item-title>{{ $t('userRolesManagement') }}</v-list-item-title>
+        </v-list-item>
       </template>
-
-      <template #[`item.updateTime`]="{ item }">
-        {{ $dayjs(item.updateTime).format() }}
-      </template>
-
-      <template #[`item.actions`]="{ item }">
-        <div
-          :class="$vuetify.breakpoint.xs?''
-            :$vuetify.theme.dark ?'v-sheet theme--dark elevation-1'
-              :'v-sheet theme--light elevation-1'"
-        >
-          <v-icon
-            color="accent"
-            class="mr-2"
-            @click="editItem(item)"
-          >
-            mdi-pencil
-          </v-icon>
-          <v-icon
-            color="error"
-            class="mr-2"
-            @click="deleteItem(item)"
-          >
-            mdi-delete-forever
-          </v-icon>
-          <v-menu
-            offset-y
-            open-on-hover
-          >
-            <template #activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                dark
-                v-bind="attrs"
-                icon
-                v-on="on"
-              >
-                <v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list dense>
-              <v-list-item @click="resetItemPassword(item)">
-                <v-list-item-avatar size="16">
-                  <v-icon small>
-                    mdi-lock
-                  </v-icon>
-                </v-list-item-avatar>
-                <v-list-item-title>{{ $t('resetPassword') }}</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="updateItemRoles(item)">
-                <v-list-item-avatar size="16">
-                  <v-icon small>
-                    mdi-account-multiple
-                  </v-icon>
-                </v-list-item-avatar>
-                <v-list-item-title>{{ $t('userRolesManagement') }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </div>
-      </template>
-    </v-data-table>
+    </or-base-data-table>
     <or-base-dialog
       ref="userDialog"
       loading
@@ -228,20 +119,34 @@
 <script>
 export default {
   name: 'OrDataTableUpmsUser',
+  props: {
+    /**
+     * 选中返回完整Object数组，默认只返回uuid数组
+     */
+    selectReturnObject: {
+      type: Boolean,
+      default: false
+    },
+    singleSelect: {
+      type: Boolean,
+      default: false
+    },
+    showSelect: {
+      type: Boolean,
+      default: false
+    },
+    showActionsWhenSelecting: {
+      type: Boolean,
+      default: false
+    }
+  },
   data () {
     return {
-      options: {},
       searchParams: {
         email: null,
         orNumber: null,
         username: null
       },
-      dataTableParams: {
-        loading: true,
-        items: [],
-        totalItems: 0
-      },
-
       selectedIndex: -1,
       editedItem: {
         uuid: null,
@@ -270,34 +175,17 @@ export default {
     // 放在这为了支持国际化，如果放在data下切换语言不会更新
     headers () {
       return [
-        { text: 'UUID', value: 'uuid', sortable: false },
         { text: this.$t('email'), value: 'email', sortable: false },
         { text: this.$t('orNumber'), value: 'orNumber', sortable: false },
         { text: this.$t('username'), value: 'username', sortable: false },
-        { text: this.$t('enabled'), value: 'enabled', sortable: false },
-        { text: this.$t('createdTime'), value: 'createdTime', sortable: false, width: '220' },
-        { text: this.$t('createBy'), value: 'createBy', sortable: false },
-        { text: this.$t('updateTime'), value: 'updateTime', sortable: false, width: '220' },
-        { text: this.$t('updateBy'), value: 'updateBy', sortable: false },
-        {
-          text: this.$t('dataTable.actions'),
-          value: 'actions',
-          sortable: false,
-          align: 'center',
-          class: 'sticky-right',
-          cellClass: 'sticky-right'
-        }
+        { text: this.$t('enabled'), value: 'enabled', sortable: false }
       ]
     },
     action () {
       return this.selectedIndex === -1 ? 'create' : 'update'
     }
   },
-  watch: {
-    options () {
-      this.getItems()
-    }
-  },
+  watch: {},
   created () {
   },
   mounted () {
@@ -326,7 +214,6 @@ export default {
     },
     resetItemPassword (item) {
       this.showPassword = false
-      this.selectedIndex = this.dataTableParams.items.indexOf(item)
       this.selectedItem = Object.assign({}, item)
       this.editedItem = Object.assign({}, this.defaultItem)
       this.selectedItem.password = null
@@ -339,7 +226,6 @@ export default {
         this.$apis.upms.user.resetPassword(this.selectedItem.uuid, this.editedItem.password)
           .then(() => {
             this.$refs.resetPasswordDialog.close()
-            this.getItems()
           }).catch(() => {
             this.$refs.resetPasswordDialog.cancelLoading()
           })
@@ -359,7 +245,7 @@ export default {
         this.$apis.upms.user[action](this.editedItem)
           .then(() => {
             this.$refs.userDialog.close()
-            this.getItems()
+            this.$refs.dataTable.getItems()
           })
           .catch(() => {
             // 取消loading
@@ -370,37 +256,28 @@ export default {
         this.$refs.userDialog.cancelLoading()
       }
     },
-    insertItem () {
+    onInsertItem () {
       this.selectedIndex = -1
       this.selectedItem = Object.assign({}, this.defaultItem)
       this.$refs.userDialog.show()
     },
-    deleteItem (item) {
-      this.selectedIndex = this.dataTableParams.items.indexOf(item)
+    onDeleteItem ({ item, index }) {
       this.selectedItem = Object.assign({}, item)
-      this.$dialog({
-        content: this.$t('deleteDialog.content', [this.selectedItem.uuid]),
-        loading: true
-      }).then((dialog) => {
-        // 删除
-        this.$apis.upms.user.delete(this.selectedItem.uuid)
-          .then(() => {
-            // 手动关闭对话框
-            this.getItems()
-            dialog.cancel()
-            this.$snackbar.success(this.$t('deleteSuccess'))
-          })
-          .catch(() => {
-            dialog.cancel()
-          })
-      })
+      // 删除
+      this.$apis.upms.user.delete(this.selectedItem.uuid)
+        .then(() => {
+          this.$refs.dataTable.deleteSuccessfully()
+        })
+        .catch(() => {
+          this.$refs.dataTable.deleteFailed()
+        })
     },
-    editItem (item) {
-      this.selectedIndex = this.dataTableParams.items.indexOf(item)
+    onEditItem ({ item, index }) {
+      this.selectedIndex = index
       this.selectedItem = Object.assign({}, item)
       this.$refs.userDialog.show()
     },
-    getItems () {
+    onGetItems ({ options, offset, limit }) {
       /* TODO 排序支持
       options:
         groupBy: Array(0)
@@ -412,25 +289,15 @@ export default {
         sortBy: Array(1)
         sortDesc: Array(1)
        */
-      const options = this.options
-      this.dataTableParams.loading = true
-      this.$apis.upms.user.list(
-        options.page === 1 ? 0 : this.dataTableParams.items.length,
-        options.itemsPerPage,
-        this.searchParams
+      this.$apis.upms.user.list(offset, limit, this.searchParams
       ).then(({ data }) => {
-        this.dataTableParams = {
-          loading: false,
-          items: data.list,
-          totalItems: data.total
-        }
+        this.$refs.dataTable.loadSuccessfully(data.list, data.total)
       }).catch(() => {
-        this.dataTableParams.loading = false
+        this.$refs.dataTable.loadFinish()
       })
     },
-    resetSearch () {
-      this.$refs.searchForm.reset()
-      this.options = { page: 1 }
+    onItemsSelected (items) {
+      this.$emit('itemsSelected', items)
     }
   }
 }
---
+++ ordinaryroad-ui/components/or/base/DataTable.vue
@@ -0,0 +1,335 @@
+<template>
+  <v-data-table
+    :single-select="singleSelect"
+    :show-select="showSelect"
+    item-key="uuid"
+    :headers="headers"
+    :items="dataTableParams.items"
+    :options.sync="options"
+    :server-items-length="dataTableParams.totalItems"
+    :loading="dataTableParams.loading"
+    :items-per-page="20"
+    :footer-props="{
+      showFirstLastPage: true,
+      firstIcon: 'mdi-page-first',
+      lastIcon: 'mdi-page-last',
+      itemsPerPageOptions:[ 10, 20, 50, 100 ]
+    }"
+    @item-selected="onItemSelected"
+    @toggle-select-all="onToggleSelectAll"
+  >
+    <template #top>
+      <v-form ref="searchForm">
+        <v-row>
+          <slot name="searchFormBody" />
+          <v-col
+            cols="6"
+            lg="3"
+            md="4"
+          >
+            <v-btn
+              small
+              color="primary"
+              outlined
+              @click="getItems"
+            >
+              <v-icon>mdi-magnify</v-icon>
+              {{ $t('search') }}
+            </v-btn>
+            <v-btn
+              small
+              outlined
+              @click="resetSearch"
+            >
+              <v-icon>mdi-refresh</v-icon>
+              {{ $t('reset') }}
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+      <v-row class="mt-2">
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-btn
+            outlined
+            color="primary"
+            dark
+            @click="insertItem"
+          >
+            <v-icon>mdi-plus</v-icon>
+            {{ $t('insert') }}
+          </v-btn>
+          <v-btn
+            outlined
+            color="success"
+            dark
+            @click="getItems"
+          >
+            <v-icon>mdi-reload</v-icon>
+            {{ $t('refresh') }}
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-divider class="mt-2" />
+    </template>
+
+    <template
+      v-if="!$vuetify.breakpoint.xs"
+      #[`header.actions`]="{ header }"
+    >
+      <v-sheet elevation="1">
+        {{ header.text }}
+      </v-sheet>
+    </template>
+
+    <!-- 动态生成插槽 -->
+    <template v-for="slot in itemSlotNames" #[slot]="{ item }">
+      <slot :name="slot" :item="item" />
+    </template>
+
+    <template #[`item.createdTime`]="{ item }">
+      {{ $dayjs(item.createdTime).format() }}
+    </template>
+
+    <template #[`item.updateTime`]="{ item }">
+      {{ $dayjs(item.updateTime).format() }}
+    </template>
+
+    <template #[`item.actions`]="{ item }">
+      <div
+        :class="$vuetify.breakpoint.xs?''
+          :$vuetify.theme.dark ?'v-sheet theme--dark elevation-1 d-flex'
+            :'v-sheet theme--light elevation-1 d-flex'"
+      >
+        <v-btn
+          icon
+          color="accent"
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          color="error"
+          @click="deleteItem(item)"
+        >
+          <v-icon>mdi-delete-forever</v-icon>
+        </v-btn>
+
+        <slot name="actions" :item="item" />
+
+        <v-menu
+          v-if="$scopedSlots.moreActions"
+          offset-y
+          open-on-hover
+        >
+          <template #activator="{ on, attrs }">
+            <v-btn
+              class="ml-2"
+              color="primary"
+              dark
+              v-bind="attrs"
+              icon
+              v-on="on"
+            >
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list dense>
+            <slot name="moreActions" :item="item" />
+          </v-list>
+        </v-menu>
+      </div>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'OrBaseDataTable',
+  props: {
+    /**
+     * 选中返回完整Object数组，默认只返回uuid数组
+     */
+    selectReturnObject: {
+      type: Boolean,
+      default: false
+    },
+    singleSelect: {
+      type: Boolean,
+      default: false
+    },
+    showSelect: {
+      type: Boolean,
+      default: false
+    },
+    showActionsWhenSelecting: {
+      type: Boolean,
+      default: false
+    },
+
+    tableHeaders: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      deleteDialog: null,
+      options: {},
+      dataTableParams: {
+        loading: true,
+        items: [],
+        totalItems: 0
+      },
+      selectedItems: [],
+      selectedIndex: -1,
+      selectedItem: {}
+    }
+  },
+  computed: {
+    /**
+     * 获取VDataTable的名称为items.*的Slot
+     * @returns {*[]}
+     */
+    itemSlotNames () {
+      const slotNames = Object.keys(this.$scopedSlots)
+      const itemSlotNames = []
+      slotNames.forEach((slotName) => {
+        if (slotName.includes('item.')) {
+          itemSlotNames.push(slotName)
+        }
+      })
+      return itemSlotNames
+    },
+    hideActions () {
+      return this.showSelect && !this.showActionsWhenSelecting
+    },
+    // 放在这为了支持国际化，如果放在data下切换语言不会更新
+    headers () {
+      const headers = []
+      headers.push(
+        { text: 'UUID', value: 'uuid', sortable: false },
+        ...this.tableHeaders,
+        { text: this.$t('createdTime'), value: 'createdTime', sortable: false, width: '220' },
+        { text: this.$t('createBy'), value: 'createBy', sortable: false },
+        { text: this.$t('updateTime'), value: 'updateTime', sortable: false, width: '220' },
+        { text: this.$t('updateBy'), value: 'updateBy', sortable: false },
+        {
+          text: this.$t('dataTable.actions'),
+          value: 'actions',
+          sortable: false,
+          align: 'center',
+          class: 'sticky-right',
+          cellClass: 'sticky-right'
+        }
+      )
+      return this.hideActions ? this.$util.remove(headers, 'value', 'actions') : headers
+    },
+    action () {
+      return this.selectedIndex === -1 ? 'create' : 'update'
+    }
+  },
+  watch: {
+    options () {
+      this.getItems()
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+    resetSearch () {
+      this.$refs.searchForm.reset()
+      this.options = { page: 1 }
+    },
+    insertItem () {
+      this.$emit('insertItem')
+    },
+    deleteItem (item) {
+      this.selectedIndex = this.dataTableParams.items.indexOf(item)
+      this.selectedItem = Object.assign({}, item)
+      this.$dialog({
+        content: this.$t('deleteDialog.content', [this.selectedItem.uuid]),
+        loading: true
+      }).then((dialog) => {
+        this.deleteDialog = dialog
+        // 删除
+        this.$emit('deleteItem', this.selectedItem, this.selectedIndex)
+      })
+    },
+    editItem (item) {
+      this.selectedIndex = this.dataTableParams.items.indexOf(item)
+      this.selectedItem = Object.assign({}, item)
+      this.$emit('editItem', this.selectedItem, this.selectedIndex)
+    },
+    getItems () {
+      this.dataTableParams.loading = true
+      this.$emit('getItems',
+        this.options,
+        this.options.page === 1 ? 0 : this.dataTableParams.items.length,
+        this.options.itemsPerPage
+      )
+    },
+    onItemSelected ({ item, value }) {
+      if (value) {
+        this.selectedItems.push(this.selectReturnObject ? Object.assign({}, item) : item.uuid)
+      } else if (this.selectReturnObject) {
+        this.selectedItems = this.$util.remove(this.selectedItems, 'uuid', item.uuid)
+      } else {
+        this.selectedItems.splice(this.selectedItems.indexOf(item.uuid), 1)
+      }
+      this.$emit('itemsSelected', this.selectedItems)
+    },
+    onToggleSelectAll ({ items, value }) {
+      this.selectedItems = []
+      if (value) {
+        items.forEach((item) => {
+          this.selectedItems.push(this.selectReturnObject ? Object.assign({}, item) : item.uuid)
+        })
+      }
+      this.$emit('itemsSelected', this.selectedItems)
+    },
+    setLoading (loading) {
+      this.dataTableParams.loading = loading
+    },
+
+    /**
+     * 删除成功后通过$refs手动调用
+     */
+    deleteSuccessfully () {
+      if (this.deleteDialog) {
+        this.getItems()
+        this.deleteDialog.cancel()
+        this.$snackbar.success(this.$t('deleteSuccess'))
+        this.deleteDialog = null
+      }
+    },
+    /**
+     * 删除失败后通过$refs手动调用
+     */
+    deleteFailed () {
+      if (this.deleteDialog) {
+        this.deleteDialog.cancel()
+        this.deleteDialog = null
+      }
+    },
+    /**
+     * 加载完成后通过$refs手动调用
+     */
+    loadSuccessfully (items, totalItems) {
+      this.dataTableParams = { loading: false, items, totalItems }
+    },
+    /**
+     * 仅取消加载动画
+     */
+    loadFinish () {
+      this.setLoading(false)
+    }
+  }
+}
+</script>
