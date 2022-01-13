d8ebb09bcc5358ea650a9005a0a537cdb4244776
Miao JinZhou <1962247851@qq.com>
2022-01-13 16:05:17 +0800

    feat(ordinaryroad-ui): 增加文件管理界面

--- ordinaryroad-ui/plugins/api/upms/file/index.js
+++ ordinaryroad-ui/plugins/api/upms/file/index.js
@@ -6,11 +6,15 @@ export default {
   },
   upload: ({ bucketName, file }) => {
     const data = new FormData()
-    data.append('bucketName', bucketName)
+    bucketName && data.append('bucketName', bucketName)
     data.append('file', file)
     return $axios({ url: '/upms/file/upload', method: 'post', data })
   },
   getFileDownloadPath: (bucketAndObjectName) => {
     return '/api/upms/file/download' + bucketAndObjectName
+  },
+  list: (offset, limit, { bucketName, objectName, originalFilename }) => {
+    const data = { limit, offset, bucketName, objectName, originalFilename }
+    return $axios({ url: '/upms/file/list', method: 'post', data })
   }
 }
