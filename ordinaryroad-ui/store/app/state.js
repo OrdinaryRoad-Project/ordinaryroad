f26d77d4fb8fb9f0466a382be0712a09bf1fed09
Miao JinZhou <1962247851@qq.com>
2022-01-16 17:31:11 +0800

    feat(ordinaryroad-ui): 菜单更新

--- ordinaryroad-ui/store/app/state.js
+++ ordinaryroad-ui/store/app/state.js
@@ -7,59 +7,98 @@ export default () => ({
   selectedThemeOption: 1,
   userMenuItems: [
     {
-      titleKey: 'userMenuTitles.userProfile',
-      to: '/user/profile',
-      icon: 'mdi-account'
+      children: [
+        {
+          titleKey: 'userMenuTitles.userProfile',
+          to: '/user/profile',
+          icon: 'mdi-account'
+        },
+        {
+          titleKey: 'logout',
+          icon: 'mdi-logout'
+        }
+      ]
     }
   ],
+  /**
+   * 最多三层
+   */
   menuItems: [
     {
-      titleKey: 'menuTitles.userManagement',
-      to: '/upms/user',
-      icon: 'mdi-account',
-      meta: {
-        permission: 'upms:user:list'
-      }
+      titleKey: 'menuTitles.auth',
+      icon: 'mdi-cog',
+      children: [
+        {
+          titleKey: 'menuTitles.registeredClientManagement',
+          to: '/auth/registered_client',
+          icon: 'mdi-account-multiple',
+          meta: {
+            permission: 'auth:registered_client:list'
+          }
+        },
+        {
+          titleKey: 'menuTitles.openidManagement',
+          to: '/auth/openid',
+          icon: 'mdi-account-multiple',
+          meta: {
+            permission: 'auth:openid:list'
+          }
+        }
+      ]
     },
     {
-      titleKey: 'menuTitles.roleManagement',
-      to: '/upms/role',
-      icon: 'mdi-account-multiple',
-      meta: {
-        permission: 'upms:role:list'
-      }
-    },
-    {
-      titleKey: 'menuTitles.permissionManagement',
-      to: '/upms/permission',
-      icon: 'mdi-lock',
-      meta: {
-        permission: 'upms:permission:list'
-      }
-    },
-    {
-      titleKey: 'menuTitles.requestPathManagement',
-      to: '/upms/request_path',
-      icon: 'mdi-menu',
-      meta: {
-        permission: 'upms:request_path:list'
-      }
-    },
-    {
-      titleKey: 'menuTitles.dictManagement',
-      to: '/upms/dict',
-      icon: 'mdi-book-cog',
-      meta: {
-        permission: 'upms:dict:list'
-      }
-    },
-    {
-      titleKey: 'menuTitles.fileManagement',
-      to: '/upms/file',
-      icon: 'mdi-file-multiple',
-      meta: {
-        permission: 'upms:file:list'
-      }
+      titleKey: 'menuTitles.upms',
+      icon: 'mdi-cog',
+      children: [
+        {
+          titleKey: 'menuTitles.userManagement',
+          to: '/upms/user',
+          icon: 'mdi-account',
+          meta: {
+            permission: 'upms:user:list'
+          }
+        },
+        {
+          titleKey: 'menuTitles.roleManagement',
+          to: '/upms/role',
+          icon: 'mdi-account-multiple',
+          meta: {
+            permission: 'upms:role:list'
+          }
+        },
+        {
+          titleKey: 'menuTitles.permissionManagement',
+          to: '/upms/permission',
+          icon: 'mdi-lock',
+          meta: {
+            permission: 'upms:permission:list'
+          }
+        },
+        {
+          titleKey: 'menuTitles.requestPathManagement',
+          to: '/upms/request_path',
+          icon: 'mdi-menu',
+          meta: {
+            permission: 'upms:request_path:list'
+          }
+        },
+        {
+          titleKey: 'menuTitles.dictManagement',
+          to: '/upms/dict',
+          icon: 'mdi-book-cog',
+          meta: {
+            permission: 'upms:dict:list'
+          }
+        },
+        {
+          titleKey: 'menuTitles.fileManagement',
+          to: '/upms/file',
+          icon: 'mdi-file-multiple',
+          meta: {
+            permission: 'upms:file:list'
+          }
+        }
+      ]
     }
   ],
   accessibleMenuItems: [],
