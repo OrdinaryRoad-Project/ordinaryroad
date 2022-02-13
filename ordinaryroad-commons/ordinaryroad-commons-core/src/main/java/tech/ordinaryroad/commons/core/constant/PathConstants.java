b0822e0afe9ed797a99ba4006dd63ff67d68361b
Miao JinZhou <1962247851@qq.com>
2022-02-13 14:14:48 +0800

    feat(commons-core, commons-mybatis, ordinaryroad-gateway): 优化处理免登接口权限校验和DO类填充逻辑

--- ordinaryroad-commons/ordinaryroad-commons-core/src/main/java/tech/ordinaryroad/commons/core/constant/PathConstants.java
+++ ordinaryroad-commons/ordinaryroad-commons-core/src/main/java/tech/ordinaryroad/commons/core/constant/PathConstants.java
@@ -36,8 +36,18 @@ public class PathConstants {
     public static final String IM_MIMC_CALLBACK = "/mimc/callback";
 
     /**
-     * 免登接口地址
+     * 创建免登接口地址
      * TODO 内部接口、免登、登录、免登或登录
      */
-    public static final List<String> NO_LOGIN_PATHS = Arrays.asList(UPMS_FILE_UPLOAD, IM_MIMC_CALLBACK);
+    public static final List<String> NO_LOGIN_CREATE_PATHS = Arrays.asList(UPMS_FILE_UPLOAD, IM_MIMC_CALLBACK);
+
+
+    public static final String TTH_MSG_UPDATE_READ = "/msg/update/read";
+    public static final String TTH_MSG_UPDATE_RECALLED = "/msg/update/recalled";
+
+    /**
+     * 更新免登接口地址
+     * TODO 内部接口、免登、登录、免登或登录
+     */
+    public static final List<String> NO_LOGIN_UPDATE_PATHS = Arrays.asList(TTH_MSG_UPDATE_READ, TTH_MSG_UPDATE_RECALLED);
 }
