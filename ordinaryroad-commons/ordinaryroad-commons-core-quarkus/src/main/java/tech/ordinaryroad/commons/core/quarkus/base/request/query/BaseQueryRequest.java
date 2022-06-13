59db0b0658443b0bee2ac766a5d6869bc4274664
Miao JinZhou <1962247851@qq.com>
2022-06-13 15:50:40 +0800

    feat: 增加quarkus通用包

---
+++ ordinaryroad-commons/ordinaryroad-commons-core-quarkus/src/main/java/tech/ordinaryroad/commons/core/quarkus/base/request/query/BaseQueryRequest.java
@@ -0,0 +1,107 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 苗锦洲
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package tech.ordinaryroad.commons.core.quarkus.base.request.query;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.eclipse.microprofile.openapi.annotations.media.SchemaProperty;
+import tech.ordinaryroad.commons.core.quarkus.base.request.BaseRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 查询请求抽象类
+ *
+ * @author mjz
+ * @date 2021/9/3
+ */
+@Getter
+@Setter
+public class BaseQueryRequest extends BaseRequest {
+
+    private static final long serialVersionUID = 5282750669538995716L;
+
+    /**
+     * 主键uuid
+     */
+    @SchemaProperty(name = "主键uuid")
+    private String uuid;
+
+    /**
+     * 创建者OR账号
+     */
+    @SchemaProperty(name = "创建者OR账号")
+    private String createBy;
+
+    /**
+     * 更新者OR账号
+     */
+    @SchemaProperty(name = "更新者OR账号")
+    private String updateBy;
+
+    /**
+     * 主键uuid列表
+     */
+    @SchemaProperty(name = "主键uuid列表")
+    private List<String> uuids;
+
+    /**
+     * 个数
+     */
+    @SchemaProperty(name = "个数")
+    private Integer limit = 20;
+
+    /**
+     * 偏移量
+     */
+    @SchemaProperty(name = "偏移量")
+    private Integer offset = 0;
+
+    /**
+     * 升序字段列表
+     */
+    @SchemaProperty(name = "升序字段列表")
+    private List<String> orderBy;
+
+    /**
+     * 降序字段列表
+     */
+    @SchemaProperty(name = "降序字段列表")
+    private List<String> orderByDesc;
+
+    /**
+     * 起始时间
+     */
+    @SchemaProperty(name = "起始时间")
+    private LocalDateTime startTime;
+
+    /**
+     * 结束时间
+     */
+    @SchemaProperty(name = "结束时间")
+    private LocalDateTime endTime;
+
+}
---
+++ ordinaryroad-commons/ordinaryroad-commons-core-quarkus/src/main/java/tech/ordinaryroad/commons/core/quarkus/base/result/IBaseResult.java
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 苗锦洲
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package tech.ordinaryroad.commons.core.quarkus.base.result;
+
+
+import java.io.Serializable;
+
+/**
+ * 结果接口
+ *
+ * @author mjz
+ * @date 2021/9/3
+ */
+public interface IBaseResult<T> extends Serializable {
+
+    /**
+     * 返回code
+     *
+     * @return code
+     */
+    int getCode();
+
+    /**
+     * 得到消息
+     *
+     * @return msg
+     */
+    String getMsg();
+
+    /**
+     * 得到数据
+     *
+     * @return data
+     */
+    T getData();
+
+    /**
+     * 是否成功
+     *
+     * @return success
+     */
+    boolean getSuccess();
+
+    /**
+     * 异常详情
+     *
+     * @return 异常详情
+     */
+    String getDetails();
+
+    void setCode(int code);
+
+    void setMsg(String msg);
+
+    void setSuccess(boolean success);
+
+    void setData(T data);
+
+    void setDetails(String details);
+}
