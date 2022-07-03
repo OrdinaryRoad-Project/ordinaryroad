d9297524eada93972a9378774e0fa1a841f45b21
Miao JinZhou <1962247851@qq.com>
2022-07-03 22:27:36 +0800

    feat(commons-core-quarkus): 增加请求体字段类型为LocalDateTime的转换器

--- ordinaryroad-commons/ordinaryroad-commons-core-quarkus/src/main/java/tech/ordinaryroad/commons/core/quarkus/base/request/query/BaseQueryRequest.java
+++ ordinaryroad-commons/ordinaryroad-commons-core-quarkus/src/main/java/tech/ordinaryroad/commons/core/quarkus/base/request/query/BaseQueryRequest.java
@@ -29,6 +29,9 @@ import lombok.Setter;
 import org.eclipse.microprofile.openapi.annotations.media.SchemaProperty;
 import tech.ordinaryroad.commons.core.quarkus.base.request.BaseRequest;
 
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.QueryParam;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -42,66 +45,77 @@ import java.util.List;
 @Setter
 public class BaseQueryRequest extends BaseRequest {
 
-    private static final long serialVersionUID = 5282750669538995716L;
+    private static final long serialVersionUID = -619594620770230224L;
 
     /**
      * 主键uuid
      */
     @SchemaProperty(name = "主键uuid")
+    @FormParam("uuid")
     private String uuid;
 
     /**
      * 创建者OR账号
      */
     @SchemaProperty(name = "创建者OR账号")
+    @FormParam("createBy")
     private String createBy;
 
     /**
      * 更新者OR账号
      */
     @SchemaProperty(name = "更新者OR账号")
+    @FormParam("updateBy")
     private String updateBy;
 
     /**
      * 主键uuid列表
      */
     @SchemaProperty(name = "主键uuid列表")
+    @FormParam("uuids")
     private List<String> uuids;
 
     /**
      * 个数
      */
     @SchemaProperty(name = "个数")
+    @DefaultValue("20")
+    @QueryParam("limit")
     private Integer limit = 20;
 
     /**
      * 偏移量
      */
     @SchemaProperty(name = "偏移量")
+    @QueryParam("offset")
     private Integer offset = 0;
 
     /**
      * 升序字段列表
      */
     @SchemaProperty(name = "升序字段列表")
+    @FormParam("orderBy")
     private List<String> orderBy;
 
     /**
      * 降序字段列表
      */
     @SchemaProperty(name = "降序字段列表")
+    @FormParam("orderByDesc")
     private List<String> orderByDesc;
 
     /**
      * 起始时间
      */
     @SchemaProperty(name = "起始时间")
+    @FormParam("startTime")
     private LocalDateTime startTime;
 
     /**
      * 结束时间
      */
     @SchemaProperty(name = "结束时间")
+    @FormParam("endTime")
     private LocalDateTime endTime;
 
 }
