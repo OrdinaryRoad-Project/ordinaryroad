997ab264de41d85316e783ab0d82e09bea265486
Miao JinZhou <or-mjz@qq.com>
2022-11-23 11:25:19 +0800

    build: 版本号更新为1.4.0

--- ordinaryroad-docs/docs/.vuepress/config.js
+++ ordinaryroad-docs/docs/.vuepress/config.js
@@ -40,7 +40,6 @@ module.exports = {
                 ]
             },
             {text: '在线演示', link: 'https://ordinaryroad.tech:8021'},
-            {text: 'GitHub', link: 'https://github.com/1962247851/ordinaryroad'},
             /* TODO 历史版本 {
                 text: 'v1.3.2',
                 // ariaLabel: 'Language Menu',
@@ -77,6 +76,24 @@ module.exports = {
                 ]
             }
         ],
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: '1962247851/ordinaryroad',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: 'GitHub',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: '1962247851/ordinaryroad',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'ordinaryroad-docs/docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'dev',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '帮助我们改善此页面！'
     },
     plugins: ['@vuepress/medium-zoom']
 }
