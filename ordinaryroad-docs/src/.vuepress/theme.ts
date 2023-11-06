68ebeb8cb84c1fc4a03ba0316d30872c0de1cc55
Miao JinZhou <or-mjz@qq.com>
2023-11-06 16:26:47 +0800

    docs: 增加公安备案信息

--- ordinaryroad-docs/src/.vuepress/theme.ts
+++ ordinaryroad-docs/src/.vuepress/theme.ts
@@ -49,7 +49,7 @@ export default hopeTheme({
     // sidebar
     sidebar,
 
-    footer: "©2019-present OrdinaryRoad 版权所有 <a href='https://beian.miit.gov.cn/' target='_blank'>苏ICP备2020055920号-2</a>",
+    footer: "©2019-present OrdinaryRoad 版权所有<br/><div style='display: flex; align-content: center'><img src='/gongan.png' alt='备案图标'/><a href='https://beian.mps.gov.cn/#/query/webSearch?code=32021102001551' target='_blank' style='margin-left: 2px'>苏公网安备32021102001551号</a><a href='https://beian.miit.gov.cn/' target='_blank' style='margin-left: 4px'>苏ICP备2020055920号-2</a></div>",
     copyright: false,
     displayFooter: true,
 
